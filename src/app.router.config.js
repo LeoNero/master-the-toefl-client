3b55c0826082cbac39a84aa21409a427ccfa30ba
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2016-09-28 00:03:35 -0300

    Added loader

--- src/app.router.config.js
+++ src/app.router.config.js
@@ -14,12 +14,40 @@ export default class {
       config.addPipelineStep('authorize', AuthorizeStep);
 
       config.map([
-        { route: ['', 'home', 'home/:page'],  name: 'home',   moduleId: 'views/home/index',   nav: true },
-        { route: 'logout',          name: 'logout',          moduleId: 'views/logout/index' },
-        { route: 'speaking/new',    name: 'new-speaking', moduleId: 'views/speaking/new/index', title: 'New Speaking', auth: true },
-        { route: 'speaking/:shortId',   name: 'show-speaking', moduleId: 'views/speaking/show/index' },
-        { route: 'profile/me',          name: 'profile-me', moduleId: 'views/profile/show/index', auth: true },
-        { route: 'contact',             name: 'contact', moduleId: 'views/contact/index' }
+        { 
+          route: ['', 'home', 'home/:page'],  
+          name: 'home',   
+          moduleId: 'views/home/index',   
+          nav: true 
+        },
+        { 
+          route: 'logout',          
+          name: 'logout',          
+          moduleId: 'views/logout/index' 
+        },
+        { 
+          route: 'speaking/new',    
+          name: 'new-speaking', 
+          moduleId: 'views/speaking/new/index', 
+          title: 'New Speaking', 
+          auth: true 
+        },
+        { 
+          route: 'speaking/:shortId',   
+          name: 'show-speaking', 
+          moduleId: 'views/speaking/show/index' 
+        },
+        { 
+          route: 'profile/me',          
+          name: 'profile-me', 
+          moduleId: 'views/profile/show/index', 
+          auth: true 
+        },
+        { 
+          route: 'contact',             
+          name: 'contact', 
+          moduleId: 'views/contact/index' 
+        }
       ]);
     }
 
